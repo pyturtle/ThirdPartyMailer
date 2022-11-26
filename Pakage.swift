e931e4f96c9396616665b447724068ebe4194b31
Nathan <Nathan-nguyen@outlook.com>
2022-11-26 03:48:52 -0500

    Create Pakage.swift

---
+++ Pakage.swift
@@ -0,0 +1,20 @@
+
+
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "ThirdPartyMailer",
+    platforms: [.iOS(.v10)],
+    products: [
+        .library(name: "ThirdPartyMailer", targets: ["ThirdPartyMailer"]),
+    ],
+    targets: [
+        .target(
+            name: "ThirdPartyMailer",
+            dependencies: []),
+        .testTarget(
+            name: "ThirdPartyMailerTests",
+            dependencies: ["ThirdPartyMailer"]),
+    ]
+)
